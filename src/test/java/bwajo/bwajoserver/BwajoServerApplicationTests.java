aaed05791e581af77163aa1e0b9cf22d88d95059
oxxultus <a38027928@gmail.com>
2025-03-27 23:20:18 +0900

    Github connecting test

---
+++ src/test/java/bwajo/bwajoserver/BwajoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bwajo.bwajoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BwajoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
